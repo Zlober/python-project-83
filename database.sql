b62134d03cbfd173435bd3815f07ff8f98c8505e
Sachkov Ilya <sos31337@gmail.com>
2023-01-07 01:33:55 +0300

    Minorr change

--- database.sql
+++ database.sql
@@ -1,11 +1,13 @@
 BEGIN;
-CREATE DATABASE pages;
-    CREATE TABLE urls (
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE urls (
         id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name varchar(255),
         created_at date
     );
-    CREATE TABLE url_checks (
+CREATE TABLE url_checks (
         id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         url_id bigint REFERENCES urls(id),
         status_code int,
